f48e1b7911a840932635ea605bddac2420784ddd
Merajul Rahman <shiponrbm@gmail.com>
2025-08-07 15:20:10 +0600

    CSS Moduels

--- First-Project/src/CSSModules/CSSModules.jsx
+++ First-Project/src/CSSModules/CSSModules.jsx
@@ -1,8 +1,11 @@
+import styles from './CSSModules.module.css'
 function CSSModules()
 {
     return (
-        <div>
-            <h3>This is an example of </h3>
+        <div className={styles.block}>
+            <h3 className={styles.heading}>This is an example of CSS using Modules</h3>
+            <p className={styles.text}>I will style this paragraph using class of .modules.css file</p>
+
         </div>
     );
 }
