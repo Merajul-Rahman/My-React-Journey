ec41eaccb02af40684d2ec88a4c0c4ae9406b44a
Merajul Rahman <shiponrbm@gmail.com>
2025-08-07 15:06:13 +0600

    Setup-React-Project

---
+++ First-Project/src/App.jsx
@@ -0,0 +1,5 @@
+function App()
+{
+  return <h1>My First Component</h1>
+}
+export default App
---
+++ First-Project/src/CSSModules/CSSModules.jsx
@@ -0,0 +1,9 @@
+function CSSModules()
+{
+    return (
+        <div>
+            <h3>This is an example of </h3>
+        </div>
+    );
+}
+export default CSSModules
